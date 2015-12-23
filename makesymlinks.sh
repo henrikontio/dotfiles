f9821fff7d8d38f1c5663b1f8f5fa9efc96320e7
Henri Kontio <Henri.Kontio@solita.fi>
2015-12-23 12:07:24 +0200

    Add mercurial dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="tmux tmux.conf tmuxinator zprofile zshrc gitconfig gitignore_global"    # list of files/folders to symlink in homedir
+files="tmux tmux.conf tmuxinator zprofile zshrc gitconfig gitignore_global hgrc hgignore_global"    # list of files/folders to symlink in homedir
 
 ##########
 
